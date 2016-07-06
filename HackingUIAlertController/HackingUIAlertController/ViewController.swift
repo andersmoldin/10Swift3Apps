7928acaa2e779f44bc71722bd4df03f89cbb3bf3
Anders Moldin <anders@moldin.com>
2016-07-06 12:02:53 +0200

    Added icons to PlayWithAttributes and a new project: HackingUIALertController

---
+++ HackingUIAlertController/HackingUIAlertController/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  HackingUIAlertController
+//
+//  Created by Anders Moldin on 2016-07-06.
+//  Copyright © 2016 Anders Moldin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pressButton(_ sender: AnyObject) {
+        
+        
+//        let alertController = UIAlertController(title: "Alert title", message: "This is a testing message", preferredStyle: UIAlertControllerStyle.alert)
+//        
+//        let backView = alertController.view.subviews.last?.subviews.last
+//        backView?.layer.cornerRadius = 10.0
+//        backView?.backgroundColor = UIColor.yellow()
+//        
+//        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+//        alertController.addAction(action)
+//        
+//        self.present(alertController, animated: true, completion: nil)
+    }
+
+}
+
